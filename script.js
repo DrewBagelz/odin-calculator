9a9156375dd5961565b906d92d565d7b29c9f5d8
Andrew Livigni <152040968+DrewBagelz@users.noreply.github.com>
2024-08-13 23:32:29 -0400

    Write operate function and divide by zero error

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+let operandA;
+let operator;
+let operandB;
+
+function operate(operandA, operator, operandB) {
+	if (operator === "+") {
+		return addition(operandA, operandB);
+	} else if (operator === "-") {
+		return subtraction(operandA, operandB);
+	} else if (operator === "*") {
+		return multiplication(operandA, operandB);
+	} else if (operator === "/") {
+		return division(operandA, operandB);
+	}
+}
+
 function addition(a, b) {
 	return a + b;
 }
@@ -11,5 +27,9 @@ function multiplication(a, b) {
 }
 
 function division(a, b) {
-	return a / b;
+	if (b === 0) {
+		return "You can't divide by 0 you silly goose!";
+	} else {
+		return a / b;
+	}
 }

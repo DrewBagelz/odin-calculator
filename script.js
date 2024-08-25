06ef25154778dac62b30b7a2b4c52e821fae3e26
Andrew Livigni <152040968+DrewBagelz@users.noreply.github.com>
2024-08-25 19:30:32 -0400

    Set up querySelectors

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-let operandA;
-let operator;
-let operandB;
+let operandA = "";
+let operator = "";
+let operandB = "";
+
+const screen = document.querySelector(".screen");
+const allClear = document.querySelector("#all-clear");
+const clear = document.querySelector("#clear");
+const decimal = document.querySelector("#decimal");
+const equals = document.querySelector("#equals");
+const numbers = document.querySelectorAll(".num-button");
+const operators = document.querySelectorAll(".op-button");
 
 function operate(operandA, operator, operandB) {
 	if (operator === "+") {
